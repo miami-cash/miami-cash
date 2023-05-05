dd616b51d20b4d4f4baa30a126e7f120778f909d
LucasGoulois <lucas.goulois@decathlon.com>
2023-05-05 19:48:08 +0200

    Front and back

--- truffle/contracts/Casino.sol
+++ truffle/contracts/Casino.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity ^0.8.18;
-
 import "./Ownable.sol";
+pragma solidity ^0.8.18;
 
 contract Casino is Ownable {
     struct Player {
         address addr;
         bool isWhiteListed;
+        uint totalWin;
     }
 
     struct Constants {
@@ -15,6 +14,8 @@ contract Casino is Ownable {
         uint8 nbWinningDoor;
         uint winningMultiplier;
         uint losingMultiplier;
+        uint minBetAmount;
+        uint maxBetAmount;
     }
 
     uint loosingDoor = 0;
@@ -22,16 +23,17 @@ contract Casino is Ownable {
         nbDoor: 3,
         nbWinningDoor: 2,
         winningMultiplier: 12,
-        losingMultiplier: 8
+        losingMultiplier: 8,
+        minBetAmount: 5000000 gwei,
+        maxBetAmount: 1000000000 gwei
     });
-    enum Status {SUCCESS, LOSE, PENDING}
 
     // Tableau de la liste blanche pour stocker les informations des joueurs
-    mapping(address => Player) public whiteList;
-
+    mapping(address => Player) public casinoPlayer;
     // Evénement qui se déclenche lorsqu'un joueur fait un pari
     event BetPlaced(address indexed player, uint amount, uint door);
 
+    event BetInitiated(address indexed player, uint amount);
     // Evénement qui se déclenche lorsqu'un joueur gagne un pari
     event BetWon(address indexed player, uint amount, uint door);
 
@@ -41,68 +43,71 @@ contract Casino is Ownable {
     // Evénement qui se déclenche lorsqu'un joueur rejoint la whitelist
     event PlayerWhitelisted(address indexed player);
 
-    // Le taux multiplicateur minimal et maximal pour les paris
-    uint constant MIN_MULTIPLIER = 102;
-    uint constant MAX_MULTIPLIER = 110;
-
-    // Le montant minimal et maximal pour les paris
-    uint constant MIN_BET_AMOUNT = 0.005 ether;
-    uint constant MAX_BET_AMOUNT = 1 ether;
-
-    // Le nombre de portes
-    uint constant DOOR_COUNT = 3;
-
     // Fonction pour inscrire un joueur sur la whitelist
-    function addToWhiteList() public {
-        whiteList[msg.sender].isWhiteListed = true;
+    function addToWhiteList() public onlyOwner {
+        require(!casinoPlayer[msg.sender].isWhiteListed, "Le joueur est deja whitelist");
+        casinoPlayer[msg.sender].isWhiteListed = true;
         emit PlayerWhitelisted(msg.sender);
     }
 
     // Fonction pour retirer un joueur de la whitelist
-    function removeFromWhiteList() public {
-        whiteList[msg.sender].isWhiteListed = false;
+    function removeFromWhiteList() public onlyOwner {
+        casinoPlayer[msg.sender].isWhiteListed = false;
     }
 
     function randomPorte() public view returns (uint){
-        uint loosingdoor = uint(keccak256(abi.encodePacked(block.timestamp, block.number - 1, msg.sender))) % DOOR_COUNT;
+        uint loosingdoor = uint(keccak256(abi.encodePacked(block.timestamp, block.number - 1, msg.sender))) % constants.nbDoor;
         return loosingdoor;
     }
 
-    function resetGame() public {
+    function resetGame() internal {
         loosingDoor = 0;
     }
 
     function win(uint betAmount, uint winningMultiplier) internal {
         uint payout = betAmount * winningMultiplier / 10;
         payable(msg.sender).transfer(payout);
+        casinoPlayer[msg.sender].totalWin += (payout - betAmount);
     }
 
     function lose(uint betAmount, uint losingMultiplier) internal {
         uint payout = betAmount * losingMultiplier / 10;
         payable(msg.sender).transfer(payout);
+        casinoPlayer[msg.sender].totalWin += (payout - betAmount);
     }
 
-    function play(uint betAmount, uint choosedDoor, uint winningMultiplier, uint losingMultiplier) public payable returns (bool){
-        require(msg.value == betAmount);
-        require(loosingDoor == 0);
-        require(choosedDoor / 3 <= 1);
-        loosingDoor = randomPorte();
+    function play(uint betAmount, uint choosedDoor) public returns (bool){
+        require(choosedDoor / 3 <= 1, "la porte choisi doit etre 1,2 ou 3");
+        require(loosingDoor != 0, "le joueur doit avoir clique sur jouer");
+        emit BetPlaced(msg.sender, betAmount, choosedDoor);
         if (choosedDoor == loosingDoor) {
-            lose(betAmount, losingMultiplier);
+            emit BetLost(msg.sender, betAmount, choosedDoor);
+            lose(betAmount, constants.losingMultiplier);
+            resetGame();
             return false;
         }
         else {
-            win(betAmount, winningMultiplier);
+            emit BetWon(msg.sender, betAmount, choosedDoor);
+            win(betAmount, constants.winningMultiplier);
+            resetGame();
             return true;
         }
+    }
+
+    function initiate(uint betAmount) public payable returns (uint){
+        resetGame();
+        require(casinoPlayer[msg.sender].isWhiteListed, "Le joueur doit etre whitelisted");
+        require(loosingDoor == 0, "la porte perdante doit etre 0");
+        emit BetInitiated(msg.sender, betAmount);
+        loosingDoor = randomPorte();
+        return loosingDoor;
     }
 
     function credit() public payable {
     }
 
     function withdraw(uint amount) public payable onlyOwner {
-        require(amount <= address(this).balance);
+        require(amount <= address(this).balance, "le montant recupere doit etre inferieur au solde");
         payable(owner()).transfer(amount);
     }
-
 }
